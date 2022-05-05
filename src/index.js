b0775c80cc974d568f294bbf16450b8c504d12e3
Aibek <ai.bek@mail.ru>
2022-05-05 12:17:49 +0600

    feat: add base body fot textarea and keyboard

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './assets/styles/style.scss';
+
+const container = document.createElement('div');
+container.className = 'container';
+document.body.appendChild(container);
+
+const textareaWrapper = document.createElement('div');
+textareaWrapper.className = 'textarea-wrapper';
+container.appendChild(textareaWrapper);
+
+const textareaInput = document.createElement('textarea');
+textareaInput.className = 'textarea-input';
+textareaWrapper.appendChild(textareaInput);
+
+const keyboardBody = document.createElement('div');
+keyboardBody.className = 'keyboard-body';
+container.appendChild(keyboardBody);
